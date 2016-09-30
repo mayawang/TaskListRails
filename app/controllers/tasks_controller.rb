4068075032c8cee700180bc2271972611f53cbc6
Maya Wang <salome.neptune@gmail.com>
2016-09-30 14:26:11 -0700

    update tasks controller for all three waves logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,41 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def create
+    @params = params
+    @task = Task.new
+    @task.title = params[:task][:title]
+    @task.description = params[:task][:description]
+    @task.is_complete = params[:task][:is_complete]
+    @task.complete_at = params[:task][:complete_at]
+    @task.save
   end
 
   def show
+    @task = Task.find(params[:id].to_i)
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+    @task = Task.find(params[:id].to_i)
   end
 
   def update
+    @task = Task.find(params[:id].to_i)
+    
+    @task.title = params[:task][:title]
+    @task.description = params[:task][:description]
+    @task.is_complete = params[:task][:is_complete]
+    @task.complete_at = params[:task][:complete_at]
+    @task.save
   end
 
   def destroy
+    @task = Task.destroy(params[:id].to_i)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,13 @@ Rails.application.routes.draw do
   post 'tasks/create' => 'tasks#create', as: 'create'
 
   # return a HTML form for editing a new task
-  get 'tasks/edit'
+  get 'tasks/:id/edit' => 'tasks#edit', as: 'edit'
 
   # update a task and linke to a result page
-  get 'tasks/update'
+  get 'tasks/:id/update' => 'tasks#update', as: 'update'
 
   # delete a task and link to a result page
-  get 'tasks/destroy'
+  get 'tasks/destroy' => 'posts#destroy', as: 'delete'
 
 
 end
