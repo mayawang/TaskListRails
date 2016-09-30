f45752fe9a62bceb82b9e736b816eb7e32811afb
Maya Wang <salome.neptune@gmail.com>
2016-09-30 14:13:29 -0700

    created task controller and task model. set up routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'tasks#index'
+
+  get 'tasks/index', as:'index'
+
+  # show list of index
+  get 'tasks/show/:id' => 'tasks#show', as: 'show'
+
+  # return a HTML form for adding a new task
+  get 'tasks/new'  => 'tasks#new', as: 'new'
+
+  # create a new task and link to a result page
+  post 'tasks/create' => 'tasks#create', as: 'create'
+
+  # return a HTML form for editing a new task
+  get 'tasks/edit'
+
+  # update a task and linke to a result page
+  get 'tasks/update'
+
+  # delete a task and link to a result page
+  get 'tasks/destroy'
+
+
 end
